e789faede6f52cdf042db565920b2e96b91b7a25
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-02-10 15:04:54 +0530

    basics of js explained

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-starting js--javaScript
- var let const------3 tarike s variable declare kr sakte hn
+// starting js--javaScript
+//  var let const------3 tarike s variable declare kr sakte hn
 
-variable declaration
-var a;   /*int string float ..kuchh v ho sakta h*/
+// variable declaration
+// var a;   /*int string float ..kuchh v ho sakta h*/
 
-a=10;
+// a=10;
+// console.log(a);
+// console.log(a);
+// a= "hello";
+// console.log(a);
+// console.log(typeof a);
+// a=true;
+// console.log(typeof a);
+// var a="how you doing ";
+// console.log(typeof a);
+
+
+// use of backtick` explained
+
+
+
+// var num =`400`;
+// console.log(`half of ${num} is ${num/2}`);
+
+// var str=`what is your name
+// myname is abhishek
+// what is your age`;
+// console.log(str);
+
+// difference between ........var and let............
+
+
+// var a =200;
+// console.log(a);
+
+// var a =400;
+// console.log(a);
+
+// let l=200;
+// console.log(l);
+
+// // let l=400;
+// // console.log(l);     /*..erroraa rha hoga...var s ek variable ko kayi bar redefine kr sakte koi error nhi aata...pr let s nhi
+// l=400;
+// console.log(l);   /* /..error nhi...means we can reinitilize but cannot redeclare
+
+// const neither  reinitialize nor redeclare
+
+const a=4;
 console.log(a);
+a=6;
 console.log(a);
-a= "hello";
-console.log(a);
-console.log(typeof a);
-a=true;
-console.log(typeof a);
-var a="how you doing ";
-console.log(typeof a);
-
-
-use of backtick` explained
-
-
-
-var num =`400`;
-console.log(`half of ${num} is ${num/2}`);
-
-var str=`what is your name
-myname is abhishek
-what is your age`;
-console.log(str);
+// clear h cannot even redeclare
