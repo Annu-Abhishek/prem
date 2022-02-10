db407e7cfa26da2af22f0e22f57fa3399759c82b
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-02-10 09:14:59 +0530

    jS stated

---
+++ index.js
@@ -0,0 +1,16 @@
+// starting js--javaScript
+//  var let const------3 tarike s variable declare kr sakte hn
+
+// variable declaration
+var a;   /*int string float ..kuchh v ho sakta h*/
+
+a=10;
+console.log(a);
+console.log(a);
+a= "hello";
+console.log(a);
+console.log(typeof a);
+a=true;
+console.log(typeof a);
+
+
