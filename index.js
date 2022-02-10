9b01288570543736bb64046ec0d0d22222170933
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-02-10 13:59:35 +0530

    backtick explained

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// starting js--javaScript
-//  var let const------3 tarike s variable declare kr sakte hn
+starting js--javaScript
+ var let const------3 tarike s variable declare kr sakte hn
 
-// variable declaration
+variable declaration
 var a;   /*int string float ..kuchh v ho sakta h*/
 
 a=10;
@@ -12,5 +12,18 @@ console.log(a);
 console.log(typeof a);
 a=true;
 console.log(typeof a);
+var a="how you doing ";
+console.log(typeof a);
 
 
+use of backtick` explained
+
+
+
+var num =`400`;
+console.log(`half of ${num} is ${num/2}`);
+
+var str=`what is your name
+myname is abhishek
+what is your age`;
+console.log(str);
