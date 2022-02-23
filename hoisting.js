cdbe079ceb76c54c1f51563cb4c81b52453e1c3b
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-02-23 18:47:58 +0530

    hoisting done

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,20 @@
 console.log(a);
 greet();
+hello();
 var a=10;
 function greet (){
     console.log("HELLO");
 }
 
+var hello= function(){
+    console.log("you guys rock");
+}
+
+// ........................definition of hoisting...............
 // javascript hoistingrefers to the process whereby the interpreter appears to 
 // move the declaration of functions, 
 // variables to the top of their scope , prior to exwcution of the code.
+
+
+// ......................sir ki definition of hoisting.......
+//   we can access the variables and function even beforewe have declared it;
