20123d431aaf3ac3270e9da9b2ea8dfb8fc3f3a3
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-03-08 13:43:52 +0530

    importance of require

--- node/fs.js
+++ node/fs.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 
-
+// console.log(fs)
  //let res= fs.appendFileSync("f1.txt","Hello I am F1 file.");      /*ye file ko append krta or agr file na bna ho */
                                                                       /*  to usko bna v deta h*/
 
@@ -18,5 +18,11 @@ const fs=require("fs");
 //const fs = require("./f1.txt");       /* 1 dot ka mtlb isi folder m jo f1.txt wo chahiye */
 // console.log(abc);
 
-const abc = require("../JS/temp.js");
+const abc = require("../SORAN/temp.js");     /* 2 dot ka mtlb current working  folder s bahar niklo or dusra folder jo
+   //(v.imp. jse java m java.import.util krte hn )       SORAN nam ka folder h usme jao or usme jo file h temp nam ki uska
+                                                     access chahiye mujhe ...usme kuchh export ho rha h to wahi import hoga */
+
 console.log(abc);
+
+let ans= abc.add(2,4);         /* iss abc se hmne temp file ko access kiya usme jo object h wo v access ho gya */ 
+console.log(ans);
