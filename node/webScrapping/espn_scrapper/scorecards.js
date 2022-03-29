4432b2d7bfbcc9a509de5c4792a5bd45293a4b50
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-03-29 14:45:39 +0530

    batsman-cell selected

--- node/webScrapping/espn_scrapper/scorecards.js
+++ node/webScrapping/espn_scrapper/scorecards.js
@@ -42,9 +42,55 @@ function getMatchDetails(html) {
     console.log(opponentTeam);   
 
     // 5. get innings
-    let allBatsmenTable = selecTool(".table.batsman tbody>tr");
-    console.log(allBatsmenTable.text());
+    let allBatsmenTable = selecTool(".table.batsman tbody");
+
+    // let allBatsmenTable = selecTool(".table.batsman tbody>tr");   agr m ye krta to table ki body nhi but 15 rows aate
+                            //  note hmare pass batsaman ki table 2 h ek DC or other MI
+    
+    // console.log(allBatsmenTable.text());
+    let htmlString =""; 
+    let count = 0 ;   
+    for(let i=0; i<allBatsmenTable.length; i++){
+        htmlString += selecTool(allBatsmenTable[i]).html();     // hmne dono table k html ko add krk bdi html bnai
+       
+        // get the descendants(table columns) of each element (table rows)
+        // let allcolms = selecTool(allBatsmenTable[i]).find("td");
+        // Shikhar Dhawan  b J Yadav1513-30115.38   -> 8 columns in a row
+        // console.log(allcolms.text());
+
+        //  check to see if any of the matched elements have the given className
+//         // let hasData = selecTool(allColms[0]).hasClass();
+//     }
+//     console.log(htmlString);                // bdi html  ko print krwa diya isko copy krk ek inning.html file m table tag m wrap krk live server kra diya
+// }
+        let allRows = selecTool(allBatsmenTable[i]).find("tr");   //table m jitne v tr the sb selected =32 
+
+        for (let i = 0; i< allRows.length; i++){              // 32 times loop chlega
+            // check to see if any of the matched elements havce the given className
+
+            // if (selecTool(selecTool(allRows[i]).find("td")[0]).hasClass("batsman-cell")){
+            //         //        row i m jitne v td hn (8 columns the =8 td) usme phli column ([0]) us,e batsman-cell agr h to selecTool s select krna
+            //              //           note ...hasclass boolean h agr true rhega to isk andr jaenge false pr nhi
+            //              //to is s khali row jo thi wo select nhi hogi 32 k jagah p 15 rows hi select hogi
+            //     count ++;
+            //     console.log("inside" + count);
+            // }
+
+            let row = selecTool(allRows[i]);
+            let firstColmnOfRow = row.find("td")[0];
+            if (selecTool(firstColmnOfRow).hasClass("batsman-cell")){
+                // will be getting valid data
+                // count++;
+                //  console.log("inside" + count);
+                // name | runs |balls| 4's | 6's | sr
+            }
+        }
+    }
 }
+
+
+
+
 // visit every scorecard and get info
 module.exports = {
     gifs :getInfoFromScorecard
